5af754b2ad2a5687395a326db1be56f584a818ac
aldicar <aldidacom@gmail.com>
2022-07-11 17:25:15 -0400

    RANGOS

--- application/controllers/admin/Rangos.php
+++ application/controllers/admin/Rangos.php
@@ -8,18 +8,18 @@ class Rangos extends CI_Controller {
         //CheckUserIsAdmin();
         $this->load->model('admin/Rangomodel', 'Rangomodel');
         $this->load->helper('usuarios_helper');
-        $this->load->helper('rangos_helper');
+        //$this->load->helper('rangos_helper');
         $this->load->helper('permisos_helper');
     }
 
      function rangos()
     {
         $idUsuario = $this->session->userdata('uid_admin');
-        $idUsuario = 229;
+        $idUsuario = 46;
         $datoganancia = 5;
         $tipo = 'REN';
-        //$valor = calcularRangos($idUsuario);
-        $valor = verificarLimiteGanancias($idUsuario, $datoganancia,$tipo);
+        $valor = $this->obtenerRangos($idUsuario);
+        //$valor = verificarLimiteGanancias($idUsuario, $datoganancia,$tipo);
         echo "<br><br><br>".$valor;
     }
 
@@ -63,7 +63,7 @@ class Rangos extends CI_Controller {
         $datos = $this->Rangomodel->getUsuario();
         foreach ($datos as $fila)
         {
-            $respuesta = calcularRangos($fila->id);
+            $respuesta = $this->obtenerRangos($fila->id);
         }
         
         $resultado ='[{                 
@@ -71,5 +71,323 @@ class Rangos extends CI_Controller {
                     "mensaje":"'.$mensaje.'"
                     }]';
         echo $resultado;
+    }
+
+
+    function obtenerRangos($idUsuario,$nivel = 0,$nivelLimite = 0,$idUsarioPrincipal=0,$bonoUsarioPrincipal=0)
+    {
+      $fechaCalculo = date('Y-m-d');
+      $puntos = $this->Rangomodel->puntosUsuario($idUsuario);
+      $totalPuntos = $puntos[0]->puntos;
+      $bono = $this->valorPuntosRangosUsuario($totalPuntos);
+      //echo "nivel ".$nivel." - ".$idUsuario." - ".$nivelLimite." - ".$idUsarioPrincipal."<br>";
+      if ($nivelLimite == 0) ///ENTRA POR PRIMERA VEZ PARA OBTENER LOS VALORES DEL USUARIO INICIAL
+      {
+        //echo "PUNTO ".$totalPuntos."<br>";
+        $nivel = 1;
+        $nivelLimite = $bono;
+        $idUsarioPrincipal = $idUsuario;
+        $bonoUsarioPrincipal = $bono;
+      }
+      else
+      {
+        $nivel++;
+      }
+      //echo "BONO ".$bono." - nivel - ".$nivel." - nivel limite ".$nivelLimite." - idusuario ".$idUsuario." - usuario principal ".$idUsarioPrincipal."<br>";
+      if($bono > 0)
+      {
+        $datos = $this->Rangomodel->usuarioPatrocinador($idUsuario); 
+        if($datos)
+        {
+          //echo "nivel **** ".$nivel." - ".$nivelLimite."<br>";
+          if($nivel <= $nivelLimite)
+          { 
+            //echo $nivel." - ".$idUsuario." ---  ".json_encode($datos)."<br><br>";    
+            $calculoPorcentajes = $this->calcularPorcentajesGanancias($idUsuario,$bono,$datos,$idUsarioPrincipal,$bonoUsarioPrincipal,$nivel,$fechaCalculo);  
+          }
+              
+          foreach($datos as $fila)
+          {  
+            $this->obtenerRangos($fila->id_usuario,$nivel,$nivelLimite,$idUsarioPrincipal,$bonoUsarioPrincipal);    
+          }
+        }    
+      } 
+    }
+
+    function valorPuntosRangosUsuario($totalPuntos)
+    {
+      $valorBonos = $this->Rangomodel->puntosRangosUsuario();
+      $tamanho = count($valorBonos);
+      $bono = 0;
+
+      for($i = 0; $i < $tamanho; $i++)
+      {         
+        if($valorBonos[$i]->pontos <= $totalPuntos)
+        {        
+            $bono = $valorBonos[$i]->bono;   
+
+        }
+      }
+      return $bono;
+    }
+    function calcularPorcentajesGanancias($idUsuario,$bono,$datos,$idUsarioPrincipal,$bonoUsarioPrincipal,$nivel,$fechaCalculo)
+    {
+      $sumaGanancias = 0;
+      $gananciaBono  = 0;
+      //$fechaCalculo = date('Y-m-d');
+      $fechaRegistro = date('Y-m-d H:i:s');
+      $datoscorrelativo = $this->Rangomodel->correlativoMaximoGanancias();
+      $correlativo      = $datoscorrelativo[0]->correlativo + 1;
+      $id_rango_guardados = "";
+      $tipo_ganancia = 1;
+      //REGISTRO DE GANANCIAS POR PAQUETES
+      
+      foreach ($datos as $patrocinados)  
+      {
+        $valorpuntos = $this->Rangomodel->comprasPaqueteFacturaUsuario($patrocinados->id_usuario);    
+        foreach($valorpuntos as $puntos)
+        {
+          $idPaquete       = $puntos->id_plano;
+          $idFactura       = $puntos->id;
+          $datosPaquete    = $this->Rangomodel->datosPaqueteUsuario($idPaquete);
+          $valorPaquete    = $datosPaquete[0]->valor;
+          //$porcentaje      = $datosPaquete[0]->ganhos_diarios;
+          $gananciaDiaria  = $datosPaquete[0]->ganhos_diarios; //$valorPaquete * $porcentaje ) / 100;
+          $sumaGanancias   = $sumaGanancias + $gananciaDiaria;          
+          if($gananciaDiaria > 0)
+          {
+            $verificar = $this->Rangomodel->checkGananciasRangos($patrocinados->id_usuario,$idUsuario,$idFactura,$idPaquete,$fechaCalculo);
+            if(!$verificar)
+            {
+              $data = array(
+                  'id_usuario'      => $patrocinados->id_usuario,
+                  'id_patrocinador' => $idUsuario,
+                  'id_factura'      => $idFactura,
+                  'id_plan'         => $idPaquete,
+                  'valor_plan'      => $valorPaquete,
+                  'porcentaje'      => $porcentaje,
+                  'ganancia_diaria' => $gananciaDiaria,
+                  'correlativo'     => $correlativo,
+                  'tipo_ganancia'   => $tipo_ganancia,
+                  'fecha_calculo'   => $fechaCalculo,
+                  'fecha_registro'  => $fechaRegistro
+              );
+              $guardarGanancia = $this->Rangomodel->guardarGananciaRango($data);
+              if($guardarGanancia)
+              {
+                $id_rango_guardados = $id_rango_guardados.$guardarGanancia."|";
+              }          
+            }
+            else
+            {
+              $idRegistro = $verificar[0]->id;
+              $data = array(
+                  'id_usuario'      => $patrocinados->id_usuario,
+                  'id_patrocinador' => $idUsuario,
+                  'id_factura'      => $idFactura,
+                  'id_plan'         => $idPaquete,
+                  'valor_plan'      => $valorPaquete,
+                  'porcentaje'      => $porcentaje,
+                  'ganancia_diaria' => $gananciaDiaria,
+                  'correlativo'     => $correlativo,
+                  'tipo_ganancia'   => $tipo_ganancia,
+                  'fecha_calculo'   => $fechaCalculo,
+                  'fecha_recalculo'  => $fechaRegistro
+              );
+              $editarGanancia = $this->Rangomodel->editarGananciaRango($idRegistro,$data);
+              $id_rango_guardados = $id_rango_guardados.$idRegistro."|";
+
+            }
+          }
+        }
+      }
+      //REGISTRO DE GANANCIAS POR BONOS
+      if($sumaGanancias > 0)
+      {
+        //echo $sumaGanancias."*-*-*-*<br>";
+
+        $porcentaje = 2;
+        $gananciaBono = ($sumaGanancias * $porcentaje ) / 100;
+        $tipo_ganancia = 2;
+
+        $gananciaBono = $this->verificarLimiteGananciasPrevio($idUsuario, $gananciaBono,'REN');
+        //echo $gananciaBono." - ".$idUsuario." - <br>";
+        if($gananciaBono > 0)
+        {            
+            $verificar = $this->Rangomodel->checkGananciasRangosDirecto($idUsuario,$fechaCalculo,$tipo_ganancia);
+            if(!$verificar)
+            {
+                $gananciaBono = $this->verificarLimiteGanancias($idUsuario, $gananciaBono,'REN');
+                $data = array(
+                    'id_usuario'            => $idUsuario,              
+                    'porcentaje'            => $bono,
+                    'valor_plan'            => $sumaGanancias,
+                    'ganancia_diaria'       => $gananciaBono,
+                    'tipo_ganancia'         => $tipo_ganancia,
+                    'correlativo_ganancia'  => $correlativo,
+                    'id_rangos'             => $id_rango_guardados,
+                    'fecha_calculo'         => $fechaCalculo,
+                    'fecha_registro'        => $fechaRegistro,
+                    'nivel_ganancia'        => 1
+                );
+                $guardarGanancia = $this->Rangomodel->guardarGananciaRango($data);      
+            }
+            else
+            {
+                $idRegistro = $verificar[0]->id;
+                $data = array(
+                    'id_usuario'            => $idUsuario,              
+                    'porcentaje'            => $bono,
+                    'valor_plan'            => $sumaGanancias,
+                    'ganancia_diaria'       => $gananciaBono,
+                    'tipo_ganancia'         => $tipo_ganancia,
+                    'correlativo_ganancia'  => $correlativo,
+                    'id_rangos'             => $id_rango_guardados,
+                    'fecha_calculo'         => $fechaCalculo,
+                    'fecha_recalculo'       => $fechaRegistro,
+                    'nivel_ganancia'        => 1
+                );
+                $editarGanancia = $this->Rangomodel->editarGananciaRango($idRegistro,$data);
+            }
+            if($idUsuario != $idUsarioPrincipal)
+            {      
+                $tipo_ganancia = 3;
+                //echo $idUsarioPrincipal." entra".$gananciaBono." <br>";
+                $verificar = $this->Rangomodel->checkGananciasRangosDirecto($idUsarioPrincipal,$fechaCalculo,$tipo_ganancia);
+                if(!$verificar)
+                {                 
+                    //echo $idUsarioPrincipal." entra".$gananciaBono." <br>";   
+                    $gananciaBono = $this->verificarLimiteGanancias($idUsarioPrincipal, $gananciaBono,'REN');                       
+                    $data = array(
+                      'id_usuario'            => $idUsarioPrincipal,              
+                      'porcentaje'            => $bonoUsarioPrincipal,
+                      'valor_plan'            => $sumaGanancias,
+                      'ganancia_diaria'       => $gananciaBono,
+                      'tipo_ganancia'         => $tipo_ganancia,
+                      'correlativo_ganancia'  => $correlativo,
+                      'id_rangos'             => $id_rango_guardados,
+                      'fecha_calculo'         => $fechaCalculo,
+                      'fecha_registro'        => $fechaRegistro,
+                      'nivel_ganancia'        => $nivel
+                  );
+                  $guardarGanancia = $this->Rangomodel->guardarGananciaRango($data);      
+                }
+                else
+                {
+                  $idRegistro = $verificar[0]->id;
+                  $data = array(
+                      'id_usuario'            => $idUsarioPrincipal,              
+                      'porcentaje'            => $bonoUsarioPrincipal,
+                      'valor_plan'            => $sumaGanancias,
+                      'ganancia_diaria'       => $gananciaBono,
+                      'tipo_ganancia'         => $tipo_ganancia,
+                      'correlativo_ganancia'  => $correlativo,
+                      'id_rangos'             => $id_rango_guardados,
+                      'fecha_calculo'         => $fechaCalculo,
+                      'fecha_recalculo'       => $fechaRegistro,
+                      'nivel_ganancia'        => $nivel
+                  );
+                  $editarGanancia = $this->Rangomodel->editarGananciaRango($idRegistro,$data);
+                }                
+            }          
+        }
+      }  
+      return 1;
+    }
+
+    function verificarLimiteGananciasPrevio($id_usuario, $datoganancia,$tipo)
+    {        
+        $valorpuntos     = $this->Rangomodel->comprasPaqueteFacturaUsuario($id_usuario); 
+        $idPaquete       = $valorpuntos[0]->id_plano;
+        $datosPaquete    = $this->Rangomodel->datosPaqueteUsuario($idPaquete);    
+        $valorPaquete    = $datosPaquete[0]->valor;
+        $valorMaximoGanancia     = $valorPaquete * 2.75;
+
+        $datosUsuarios      = $this->Rangomodel->getUsuarioId($id_usuario);
+        $saldo_rendimentos  = $datosUsuarios[0]->saldo_rendimentos;
+        $saldo_indicacoes   = $datosUsuarios[0]->saldo_indicacoes;
+        $ganancias          = $saldo_rendimentos + $saldo_indicacoes; //$datosUsuarios[0]->ganancias;
+
+        $totalGanancias     = $ganancias + $datoganancia;
+
+        if($totalGanancias <= $valorMaximoGanancia)
+        {
+          if ($tipo == 'REN' )
+          {
+              $saldo_rendimentos = $saldo_rendimentos + $datoganancia;
+          }
+          else
+          {
+              $saldo_indicacoes = $saldo_indicacoes + $datoganancia;
+          }
+          $ganancias = $ganancias + $datoganancia;
+        }
+        else
+        {
+          $datoganancia = $valorMaximoGanancia - $ganancias;
+
+          if ($tipo == 'REN' )
+          {
+              $saldo_rendimentos = $saldo_rendimentos + $datoganancia;
+          }
+          else
+          {
+              $saldo_indicacoes = $saldo_indicacoes + $datoganancia;
+          }
+          $ganancias = $ganancias + $datoganancia;     
+        }    
+        return  $datoganancia;
+    }
+
+    function verificarLimiteGanancias($id_usuario, $datoganancia,$tipo)
+    {
+        $valorpuntos     = $this->Rangomodel->comprasPaqueteFacturaUsuario($id_usuario); 
+        $idPaquete       = $valorpuntos[0]->id_plano;
+        $datosPaquete    = $this->Rangomodel->datosPaqueteUsuario($idPaquete);    
+        $valorPaquete    = $datosPaquete[0]->valor;
+        $valorMaximoGanancia     = $valorPaquete * 2.75;
+
+        $datosUsuarios      = $this->Rangomodel->getUsuarioId($id_usuario);
+        $saldo_rendimentos  = $datosUsuarios[0]->saldo_rendimentos;
+        $saldo_indicacoes   = $datosUsuarios[0]->saldo_indicacoes;
+        $ganancias          = $saldo_rendimentos + $saldo_indicacoes; //$datosUsuarios[0]->ganancias;
+
+        $totalGanancias     = $ganancias + $datoganancia;
+
+        if($totalGanancias <= $valorMaximoGanancia)
+        {
+          if ($tipo == 'REN' )
+          {
+              $saldo_rendimentos = $saldo_rendimentos + $datoganancia;
+          }
+          else
+          {
+              $saldo_indicacoes = $saldo_indicacoes + $datoganancia;
+          }
+          $ganancias = $ganancias + $datoganancia;
+        }
+        else
+        {
+          $datoganancia = $valorMaximoGanancia - $ganancias;
+
+          if ($tipo == 'REN' )
+          {
+              $saldo_rendimentos = $saldo_rendimentos + $datoganancia;
+          }
+          else
+          {
+              $saldo_indicacoes = $saldo_indicacoes + $datoganancia;
+          }
+          $ganancias = $ganancias + $datoganancia;     
+        }
+        $data = array(
+          'saldo_rendimentos' => $saldo_rendimentos,
+          'saldo_indicacoes'  => $saldo_indicacoes,
+          'ganancias'         => $ganancias
+        );
+
+
+        $editarGananciaUsuarios = $this->Rangomodel->editarGananciaUsuarios($id_usuario,$data);    
+        return  $datoganancia;
     }
 }
