a8e59aef27ef9e1e958648c042f328699534d480
DIEGODAZA619 <diegodaza.da619@gmail.com>
2022-07-10 23:52:35 -0400

    CONTROLES

--- MANTENIMIENTOS/2022-07-10/instrucciones.sql
+++ MANTENIMIENTOS/2022-07-10/instrucciones.sql
@@ -7,15 +7,36 @@ SUBIR LOS SIGUIENTES ARCHIVOS
 
 application\config\routes.php
 controlador
-application\controller\admin\Addplan.php
+application\controllers\admin\Rangos.php
+application\controllers\admin\Dashboard.php
+application\controllers\admin\Usuarios.php    linea 79
+
+helpers
+application\helpers\rangos_helper.php			
+application\helpers\usuarios_helper.php       linea 403 en adelante
+
 modelo
-application\models\admin\Addplanmodel.php
+application\models\admin\Contamodel.php	      linea 36
+application\models\admin\Faturasmodel.php	  liena 273 al 279
+application\models\admin\Rangomodel.php
+application\models\admin\Usuariosmodel.php	  linea 190 en adelante
+application\models\cliente\Faturasmodel.php   linea 309 al 317
+application\models\Cronmodel.php			  linea 142 al 150   203 al 209
+application\models\admin\Addplanmodel.php     linea 59
+
 vista
 application\views\admin\addplan
+application\views\admin\rangos
+application\views\admin\usuarios\editar.php
+application\views\admin\usuarios\visualizar.php
+application\views\cliente\backoffice\backoffice.php
+
+
 js
-assets\pages\admin\usuarios.js
+assets\pages\admin\rangos.js
+assets\pages\cliente\saque.js
 
 -----------------------------------
 
-HACER LA PRUEBA CON EL USUARIO demo
+
 
