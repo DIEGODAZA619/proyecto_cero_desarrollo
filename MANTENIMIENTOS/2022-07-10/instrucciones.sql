dcea84b6a109e33e9f5673dc70b3ca1cae97d8e8
aldicar <aldidacom@gmail.com>
2022-07-10 23:20:55 -0400

    inicio

---
+++ MANTENIMIENTOS/2022-07-10/instrucciones.sql
@@ -0,0 +1,21 @@
+para los roles de usuario ejecutar el script
+
+script_2022-07-10.sql
+
+
+SUBIR LOS SIGUIENTES ARCHIVOS 
+
+application\config\routes.php
+controlador
+application\controller\admin\Addplan.php
+modelo
+application\models\admin\Addplanmodel.php
+vista
+application\views\admin\addplan
+js
+assets\pages\admin\usuarios.js
+
+-----------------------------------
+
+HACER LA PRUEBA CON EL USUARIO demo
+
